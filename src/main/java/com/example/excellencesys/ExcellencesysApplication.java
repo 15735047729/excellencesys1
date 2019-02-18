7f1e7741ba5ca016780dc56985cac8386926beef
wangkun <1368524857@qq.com>
2019-02-18 16:44:22 +0800

    二次上传

--- src/main/java/com/example/excellencesys/ExcellencesysApplication.java
+++ src/main/java/com/example/excellencesys/ExcellencesysApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-@MapperScan("cn.bdqn.excellencesys.dao")
+@MapperScan("cn.bdqn.excellencesys.*")
 public class ExcellencesysApplication {
 
 	public static void main(String[] args) {
