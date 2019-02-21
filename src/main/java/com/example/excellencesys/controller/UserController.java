3b68061a6fb62129a603e3bb3fd2b97817db4a64
xiaoping <748969397@qq.com>
2019-02-21 11:11:57 +0800

    测试1232123

--- src/main/java/com/example/excellencesys/controller/UserController.java
+++ src/main/java/com/example/excellencesys/controller/UserController.java
@@ -17,7 +17,7 @@ import org.springframework.web.bind.annotation.RestController;
 import javax.annotation.Resource;
 import java.util.List;
 
-@Api(tags = "用户1")
+@Api(tags = "用户")
 @RestController("/user")
 public class UserController {
     @Resource
